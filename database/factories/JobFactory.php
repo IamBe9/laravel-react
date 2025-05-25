4c1dcb9e680a3a29bf963b504dbe7543e5b6513a
Daniel <u2_danhru_waw@technischoosl>
2025-05-25 17:49:02 +0200

    factory

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,16 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class JobFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => $this->faker->jobTitle,
+            'salary' => $this->faker->randomElement(['$50,000 USD', '$90,000 USD', '$150,000 USD']),
+            'location' => 'Remote',
+            'schedule' => 'Full Time',
+            'url' => $this->faker->url,
+            'featured' => false,
         ];
     }
 }
--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -9,15 +9,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class TagFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->word,
         ];
     }
 }
